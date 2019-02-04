e77151ddc65b4fe7847d1220e0665b11efde957b
dannertech <danner@danner.tech>
2019-02-04 00:32:09 -0500

    styled alexa page

--- src/Pages/Alexa.js
+++ src/Pages/Alexa.js
@@ -9,6 +9,27 @@ import MyBestFriend from '../Images/Alexis-logo.png'
 import Cheerup from '../Images/cheerup.png'
 import SimpsonSpeak from '../Images/simpson-speak-donut.png'
 
+const StyledLinkButton = styled.a`
+background: black;
+color: white;
+text-decoration: none;
+border-radius: 25px;
+padding: 7px;
+box-shadow: 5px 5px grey;
+font-family: 'Raleway', sans-serif;
+
+`
+
+
+
+const StyledProjectCard = styled.div`
+margin: 27px 307px 0px 307px;
+display: flex;
+text-align: center;
+flex-direction: column;
+font-family: 'Raleway', sans-serif;
+`
+
 const StyledHeader = styled.h1`
 text-align: center;
 `
@@ -21,9 +42,33 @@ export default class Alexa extends Component {
                 <p>You're at the Alexa page</p>
                 
                     <StyledHeader>Alexa Apps</StyledHeader>
-                <div>
-                <img src=""
+                <StyledProjectCard>
+                    <div>
+                <img src={Cheerup} alt="Image of air balloon on orange background"></img>
+                <p>InspirationDaily is an Alexa app (with a compatible iOS application!) that allows the user to hear inspirational messages upon request.</p>
                 </div>
+                <div>
+                <StyledLinkButton href="https://www.amazon.com/gp/product/B07JFVNDXH?ie=UTF8&ref-suffix=ss_rw">Try it Out</StyledLinkButton>
+                </div>
+              </StyledProjectCard>
+              <StyledProjectCard>
+                    <div>
+                <img src={CatFacts} alt="Image of a cat's face against a green background"></img>
+                <p>CatFacts is an Alexa application that retrieves data from a 3rd party API to give the user a fact about cats!</p>
+                </div>
+                <div>
+                <StyledLinkButton href="https://www.amazon.com/gp/product/B07L4FLFW6?ie=UTF8&ref-suffix=ss_rw">Try it Out</StyledLinkButton>
+                </div>
+              </StyledProjectCard>
+              <StyledProjectCard>
+                    <div>
+                <img src={EightBall} alt="Image of an 8 in a camoflauge triangle"></img>
+                <p>Millennial Eight Ball was a simple yet fun Alexa app that allows the user to ask Alexa a question and Alexa responds with an answer reminiscent of an Eight Ball but in the words of a young Millennial. This app was fun and I hope to change the actual voice of Alexa!</p>
+                </div>
+                <div>
+                <StyledLinkButton href="https://www.amazon.com/gp/product/B07L4FLFW6?ie=UTF8&ref-suffix=ss_rw">Try it Out</StyledLinkButton>
+                </div>
+              </StyledProjectCard>
             </div>
         )
     }
