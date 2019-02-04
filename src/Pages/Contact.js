cfec467f154e938f7e344c59182d8d94403038db
dannertech <danner@danner.tech>
2019-02-04 01:41:29 -0500

    done

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
 import NavigationBar from '../Components/Navigation'
+import '../App.css'
+
+import styled from 'styled-components'
+
+const StyledHeader = styled.h1`
+margin: 107px 0px 0px 0px;
+text-align: center;
+`
+
+const ContactCard = styled.div`
+
+background-color: black;
+color: white;
+font-family: 'Raleway', sans-serif;
+border-radius: 15px;
+margin: 107px 507px;
+text-align: center;
+box-shadow: 5px 7px grey;
+
+`
 
 export default class Contact extends Component {
     render(){
@@ -7,7 +27,13 @@ export default class Contact extends Component {
            
             <div>
                 <NavigationBar />
-                <p>You're at the Contact page</p>
+                <StyledHeader>Hit Me Up!</StyledHeader>
+                <ContactCard id="ContactCard">
+<h1>Phone Number</h1>
+<p>(404)734-4136</p>
+<h1>Email</h1>
+<p>danner@danner.tech</p>
+                </ContactCard>
             </div>
         )
     }
